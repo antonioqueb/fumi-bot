19c1eaaca9cb94a0a1127a5cbdef1a1514bcc8e7
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-05-02 23:36:40 -0600

    Up

--- chatGPT.js
+++ chatGPT.js
@@ -3,23 +3,27 @@ require('dotenv').config();
 
 const chat = async (prompt) => {
     const url = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent';
-    const payload = {
+    const headers = {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${process.env.GEMINIPRO}`  // Asegúrate de que GEMINI_API_KEY está en tu archivo .env
+    };
+    const data = {
         contents: [{
             parts: [{ text: prompt }]
         }]
     };
-    const headers = {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${process.env.GEMINIPRO}` // Asegúrate de que GEMINIPRO está correctamente definida en tu .env
-    };
 
     try {
-        const response = await axios.post(url, payload, { headers });
+        const response = await axios.post(url, data, { headers });
         return response.data;
-    } catch (err) {
-        console.error("Error al conectar con Gemini:", err);
-        return "ERROR";
+    } catch (error) {
+        console.error('Error en la solicitud:', error.response ? error.response.data : error.message);
+        return null;
     }
 };
 
 module.exports = chat;
+
+
+
+
