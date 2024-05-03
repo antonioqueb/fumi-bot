950befbbaf38b1a1d23e5fcd1037b31473bd1dfd
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-05-03 00:42:24 -0600

    Update

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,13 @@
 FROM node:18-bullseye as bot
 WORKDIR /app
 COPY package*.json ./
+RUN npm i @google/generative-ai
 RUN npm i
 # Instalar dockerize
 RUN apt-get update && apt-get install -y wget && \
     wget https://github.com/jwilder/dockerize/releases/download/v0.6.1/dockerize-linux-amd64-v0.6.1.tar.gz && \
     tar -C /usr/local/bin -xzvf dockerize-linux-amd64-v0.6.1.tar.gz && \
-    rm dockerize-linux-amd64-v0.6.1.tar.gz && \
-    npm install @google/generative-ai
+    rm dockerize-linux-amd64-v0.6.1.tar.gz
 
 COPY . .
 ARG PUBLIC_URL
--- chatGPT.js
+++ chatGPT.js
@@ -4,14 +4,21 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 const genAI = new GoogleGenerativeAI(process.env.GEMINIPRO);
 
 async function chat(prompt) {
+  console.log('Inicializando el modelo generativo...');
   const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
   try {
     console.log('Enviando solicitud con el prompt:', prompt);
     const result = await model.generateContent(prompt);
+    console.log('Contenido generado con éxito:', result);
     return result;  // Retorna el resultado directamente
   } catch (error) {
     console.error('Error en la solicitud:', error);
+    // Posibilidad de loguear más detalles del error
+    if (error.response) {
+      console.error('Detalles de la respuesta:', error.response.data);
+      console.error('Estado de la respuesta:', error.response.status);
+    }
     return null;  // Retorna null en caso de error
   }
 }
